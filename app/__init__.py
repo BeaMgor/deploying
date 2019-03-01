2b082d50853592bbe6f050878c0cdcffed9839ac
Beatriz M Gordillo <beatriz.mgordillo@gmail.com>
2019-03-01 11:42:11 +0100

    sirve indice

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask 
 
 app = Flask(__name__, instance_relative_config=True)
+from app import views #importamos views para ejecutarlo siempre después de la instacia app
 
 app.config.from_object('config')
+
