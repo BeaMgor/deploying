df87c1cbe8e2da0f81c0f394e76d64581f63cad7
Beatriz M Gordillo <beatriz.mgordillo@gmail.com>
2019-03-03 11:12:32 +0100

    Insercion de registros

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
-from flask import render_template
+from flask import render_template,request, redirect, url_for
 from app import app
+import psycopg2
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/nuevoregistro', methods=['GET', 'POST'])
+def nuevoregistro():
+    if request.method == 'GET': 
+        return render_template('nuevoregistro.html')
+    else:
+        dbParams = app.config['DATABASE']
+        
+        conn = psycopg2.connect (host=dbParams ['host'], port=dbParams['port'], dbname=dbParams ['dbname'], user=dbParams['dbuser'], password=dbParams['password'])
+        cur = conn.cursor()
+        query = """INSERT INTO movimientos (fecha_hora, descripcion, moneda_cimprada, cantida_comprada, moneda_pagada, cantidad_pagada)
+                   VALUES ('{}', '{}', '{}', '{}', '{}', '{}');"""
+        f = request.form 
+
+        query = query.format(f['fecha'], f['hora'], f['descripcion'], f['moneda_comprada'], f['cantida_comprada'], f['moneda_pagada'], f['cantida_pagada'])
+
+        cur.execute(query)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('index'))
